0fbd5156f75965855b7a9972cb4833c320f97ed1
Diego Cantelli <diegocantelli19@gmail.com>
2021-01-24 14:11:41 -0300

    First class functions

---
+++ projeto_1/first_class_function.js
@@ -0,0 +1,9 @@
+//Funções de primeria classe são aquelas que podem ser tratadas como qualquer
+// outra variável.
+// Quando é possiível ARMAZENAR FUNÇÕES EM VARIÀVEIS
+
+let helloWorld = function () {
+    console.log('Hello World');
+}
+
+helloWorld();
