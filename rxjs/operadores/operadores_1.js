2ee3fce21025d4bf0e884a78e92214884f1cd0c3
Diego Cantelli <diegocantelli19@gmail.com>
2021-02-14 12:32:30 -0300

    Utilizando o operador from

--- rxjs/operadores/operadores_1.js
+++ rxjs/operadores/operadores_1.js
@@ -1,6 +1,6 @@
 //Operadores de criação
 const {
-    of
+    of , from
 } = require('rxjs');
 
 //Operadores de encadeamento -pipeable
@@ -9,11 +9,22 @@ const {
     map
 } = require('rxjs/operators');
 
-//O operador of emite seus valores um por vez
+//O operador of recebe n valores como parâmetro e emite seus valores um por vez
 of ('1', 'teste', false, 'último')
 //Com o last é possível obter o último dado emitido pelo of
 .pipe(
         last(),
         //map -> transforma o valor recebido para um novo valor
-        map(valor => valor.toUpperCase()))
+        map(valor => valor.toUpperCase())
+    )
+    .subscribe(valor => console.log(valor));
+
+//O operador from recebe um array como parâmetro e emite seus valores um por vez
+from(['1', 'teste', false, 'último'])
+    //Com o last é possível obter o último dado emitido pelo of
+    .pipe(
+        // last(),
+        //map -> transforma o valor recebido para um novo valor
+        // map(valor => valor.toUpperCase())
+    )
     .subscribe(valor => console.log(valor));
